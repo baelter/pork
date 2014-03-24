38b447ab5eec0c1700f96790f6116e1ecca10b87
Anders Bälter <abaelter@gmail.com>
2014-03-24 08:50:34 +0100

    jshint

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 $(function () {
+    'use strict';
     var KEY_LEFT = 37, KEY_UP = 38, KEY_RIGHT = 39, KEY_DOWN = 40,
         board, drawBoard, isBetween, isCellEmpty, addTile, tileInCell, allowedToMove, mergeTiles;
 
